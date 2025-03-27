8775a1fc89add4761f980ca8b88c8fe251e952fd
Merve Altinisik <mervealtinisik99@gmail.com>
2025-03-27 23:00:36 +0300

    first commit

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// //var generateName = require('sillyname');  // paketi talep ediyoruz
+
+// import generateName from 'sillyname';       // paketi almanın yeni versiyonu
+
+
+// var sillyName = generateName();
+
+// console.log("My name is " + sillyName);
+
+////////////////////////////////////////////////////////////
+
+
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();    // rastgele süper kahraman ismi alıyoruz
+console.log("My superhero name is " + name);
