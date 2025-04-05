8cf7b3c4b2e5ca5d70b3b01bc6169112d96f424a
Merve Altinisik <mervealtinisik99@gmail.com>
2025-04-05 22:18:08 +0300

    Created Middleware

--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -3,10 +3,14 @@ import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+app.use(morgan("combined"));
+
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
